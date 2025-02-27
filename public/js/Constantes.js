951d05832915e75ab4c5f3cbf59c8d706119b595
Castillo Ignacio <castilloignacio1510@gmail.com>
2025-02-27 10:23:13 -0300

    Tenemos un nuevo loader, un hamster dando vueltas en una rueda.

--- public/js/Constantes.js
+++ public/js/Constantes.js
@@ -44,11 +44,29 @@ export function HTML_RENDERIZADO(programas) {
 
 export function HTML_CARGANDO() {
   // Renderizar los programas
-  console.log("HTML_CARGANDO")
+  console.log("HTML_CARGANDO");
   const CONTENEDOR_DE_PROGRAMAS = document.querySelector(".contenedor");
   CONTENEDOR_DE_PROGRAMAS.classList.add("not-loaded");
   document.getElementById("formularioDeProgramas").innerHTML = `
-    <div class="loader "></div>
+
+<div role="img" class="wheel-and-hamster">
+	<div class="wheel"></div>
+	<div class="hamster">
+		<div class="hamster__body">
+			<div class="hamster__head">
+				<div class="hamster__ear"></div>
+				<div class="hamster__eye"></div>
+				<div class="hamster__nose"></div>
+			</div>
+			<div class="hamster__limb hamster__limb--fr"></div>
+			<div class="hamster__limb hamster__limb--fl"></div>
+			<div class="hamster__limb hamster__limb--br"></div>
+			<div class="hamster__limb hamster__limb--bl"></div>
+			<div class="hamster__tail"></div>
+		</div>
+	</div>
+	<div class="spoke"></div>
+</div>
   `;
   function cargarCSS(ruta) {
     const head = document.head;
